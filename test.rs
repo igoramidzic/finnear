4bd10c80a15be49b0e7f7abd47193c3d3a1a748c
Igor Amidzic <amidzicigor46@gmail.com>
2026-04-28 19:16:12 -0400

    Remove test files and associated Rust code

--- test.rs
+++
@@ -1,9 +0,0 @@
-use std::io::{stdin, stdout, Write};
-
-fn main() {
-    let mut input = String::new();
-    println!("Hello, world!");
-    
-    stdin().read_line(&mut input).unwrap();
-    println!("You said: {}", input);
-}
